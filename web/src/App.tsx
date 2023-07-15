e8a6c9096f2b9b43706d9d6235dae68bcf941c3a
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-07-15 16:13:42 +0800

    web: format code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hello seed
-    </div>
-  );
+  return <div className="App">hello seed</div>;
 }
 
 export default App;
