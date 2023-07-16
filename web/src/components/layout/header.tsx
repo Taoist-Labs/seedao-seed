f6fce634632f3bae2d0d5f9c26c5895845f8a358
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-07-16 18:07:21 +0800

    web: add logo to header

--- web/src/components/layout/header.tsx
+++ web/src/components/layout/header.tsx
@@ -1,3 +1,26 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import LogoIcon from "assets/images/logo.png";
+
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <HeaderStyle>
+      <LogoBox to="/">
+        <img src={LogoIcon} alt="" />
+      </LogoBox>
+    </HeaderStyle>
+  );
 }
+
+const HeaderStyle = styled.header`
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px solid #ddd;
+  padding: 10px 40px;
+`;
+
+const LogoBox = styled(Link)`
+  img {
+    height: 55px;
+  }
+`;
