9ffbad7b1b60885eaa11a73c4119831e0de65e53
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-07-16 18:31:59 +0800

    web: add login box to header

--- web/src/components/layout/header.tsx
+++ web/src/components/layout/header.tsx
@@ -1,13 +1,25 @@
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import LogoIcon from "assets/images/logo.png";
+import { useWeb3React } from "@web3-react/core";
+import { Button } from "@mui/material";
+import { addressToShow } from "utils/index";
+
+const LoginBox = ({ account }: { account?: string }) => {
+  if (!account) {
+    return <Button>Connect Wallet</Button>;
+  }
+  return <LoginStyle>{addressToShow(account)}</LoginStyle>;
+};
 
 export default function Header() {
+  const { account } = useWeb3React();
   return (
     <HeaderStyle>
-      <LogoBox to="/">
+      <LogoLink to="/">
         <img src={LogoIcon} alt="" />
-      </LogoBox>
+      </LogoLink>
+      <LoginBox account={account} />
     </HeaderStyle>
   );
 }
@@ -17,10 +29,17 @@ const HeaderStyle = styled.header`
   line-height: 60px;
   border-bottom: 1px solid #ddd;
   padding: 10px 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const LogoBox = styled(Link)`
+const LogoLink = styled(Link)`
   img {
     height: 55px;
+    position: relative;
+    top: 10px;
   }
 `;
+
+const LoginStyle = styled.div``;
---
+++ web/src/utils/index.ts
@@ -0,0 +1,10 @@
+export const addressToShow = (address: string, num?: number) => {
+  if (!address) return "...";
+  const n = num || 4;
+
+  const frontStr = address.substring(0, n);
+
+  const afterStr = address.substring(address.length - n, address.length);
+
+  return `${frontStr}...${afterStr}`;
+};
