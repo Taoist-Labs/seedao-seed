9af0092ef0c04c93d0831452d6f32223f09307cd
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-07-18 20:40:11 +0800

    web: add license page content

--- web/src/views/license.tsx
+++ web/src/views/license.tsx
@@ -1,3 +1,37 @@
+import styled from "@emotion/styled";
+
 export default function LicensePage() {
-  return <div>LicensePage</div>;
+  return (
+    <LicensePageStyle>
+      <Container>
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+        LicensePage LicensePage LicensePage LicensePage LicensePage LicensePage
+      </Container>
+    </LicensePageStyle>
+  );
 }
+
+const LicensePageStyle = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 140px);
+  padding: 40px 60px;
+  box-sizing: border-box;
+`;
+
+const Container = styled.div`
+  background: #eee;
+  padding: 40px;
+`;
