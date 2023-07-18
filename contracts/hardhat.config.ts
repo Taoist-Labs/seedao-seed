e7afeff095d3073ac637dff012b44c22d243a5d0
xiaosongfu <xiaosong.fu@outlook.com>
2023-07-18 16:37:06 +0800

    contract: add hardhat-publish-typechain plugin

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades"
 import "hardhat-abi-exporter";
+import "hardhat-publish-typechain";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 
@@ -30,8 +31,18 @@ const config: HardhatUserConfig = {
   },
   abiExporter: {
     runOnCompile: true,
-    pretty: false
-  }
+    pretty: false,
+  },
+  publishTypechain: {
+    name: "seed-contracts",
+    version: "0.1.0",
+    ethers: "^5.7.2",
+    typescript: "^4.9.5",
+    // pretty: false,
+    contracts: ["SeeDAO"],
+    authToken:
+      process.env.PRIVATE_KEY || "npm_pZ...yP",
+  },
 };
 
 export default config;
