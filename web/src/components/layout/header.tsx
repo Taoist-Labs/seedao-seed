f47dfdaf61c5431987f1dd4adf5f4f927d850d3d
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-08-10 21:28:27 +0800

    web: update header

--- web/src/components/layout/header.tsx
+++ web/src/components/layout/header.tsx
@@ -5,13 +5,55 @@ import { useWeb3React } from "@web3-react/core";
 import { Button } from "@mui/material";
 import { addressToShow } from "utils/index";
 import LoginModal from "components/modals/loginModal";
-import React from "react";
+import React, { useState } from "react";
 import { useAppContext, AppActionType } from "providers/appProvider";
 
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+
 import GreenStarIcon from "assets/images/home/green_star.svg";
+import MenuIcon from "assets/images/home/menu.svg";
+import LanguageIcon from "assets/images/home/language.svg";
+import { useTranslation } from "react-i18next";
+
+const SEED_OPTIONS = [
+  // { path: "about", label: "About" },
+  { path: "gallery", label: "Gallery" },
+  // { path: "license", label: "License" },
+  // { path: "shop", label: "Shop" },
+];
+
+const SmNav = ({ handleClose }: { handleClose: () => void }) => {
+  const [expand, setExpand] = useState(false);
+  return (
+    <SmMenu>
+      <div className="content">
+        <div className="top">
+          <div className="seed-menu" onClick={() => setExpand(!expand)}>
+            <span>Seed</span>
+            {expand ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </div>
+          {expand && (
+            <ul className="sub-menu">
+              {SEED_OPTIONS.map((item, i) => (
+                <li key={i}>
+                  <Link to={item.path}>{item.label}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="bottom">
+          <Languagebutton />
+          <EnterAppButton />
+        </div>
+      </div>
+      <div className="mask" onClick={handleClose}></div>
+    </SmMenu>
+  );
+};
 
 const LoginBox = ({ account }: { account?: string }) => {
   const { dispatch } = useAppContext();
@@ -31,18 +73,49 @@ const LoginBox = ({ account }: { account?: string }) => {
   return <LoginStyle to="/user">{addressToShow(account)}</LoginStyle>;
 };
 
-const SEED_OPTIONS = [
-  { path: "about", label: "About" },
-  { path: "gallery", label: "Gallery" },
-  { path: "license", label: "License" },
-  { path: "shop", label: "Shop" },
-];
+const EnterAppButton = () => {
+  return (
+    <EnterButton
+      onClick={() => window.open("https://app.seedao.xyz/", "_blank")}
+    >
+      <img src={GreenStarIcon} alt="" />
+      <span>Enter App</span>
+    </EnterButton>
+  );
+};
+
+const Languagebutton = () => {
+  const { i18n } = useTranslation();
+  const [lan, setLan] = useState<string>("en");
+
+  const getOppositeLan = () => {
+    if (i18n.language === "en") {
+      return "zh";
+    } else {
+      return "en";
+    }
+  };
+
+  const changeLan = () => {
+    const _olan = getOppositeLan();
+    i18n.changeLanguage(_olan);
+    setLan(_olan);
+  };
+
+  return (
+    <LanguageBox onClick={changeLan}>
+      <img src={LanguageIcon} alt="" />
+      <span>{lan === "zh" ? "ZH" : "EN"}</span>
+    </LanguageBox>
+  );
+};
 
 export default function Header() {
   const navigate = useNavigate();
   const { account } = useWeb3React();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const openSelect = Boolean(anchorEl);
+  const [showMenu, setShowMenu] = React.useState(false);
 
   const handleSelect = (path: string) => {
     navigate(path);
@@ -55,6 +128,9 @@ export default function Header() {
         <LogoLink to="/">
           <img src={LogoIcon} alt="" />
         </LogoLink>
+        <SmNavStyle onClick={() => setShowMenu(true)}>
+          <img src={MenuIcon} alt="" />
+        </SmNavStyle>
         <NavStyle>
           <SelectBox
             id="seed-select"
@@ -84,17 +160,67 @@ export default function Header() {
             ))}
           </Menu>
           <LoginBox account={account} />
-          <EnterButton
-            onClick={() => window.open("https://app.seedao.xyz/", "_blank")}
-          >
-            <img src={GreenStarIcon} alt="" />
-            <span>Enter App</span>
-          </EnterButton>
+          <Languagebutton />
+          <EnterAppButton />
         </NavStyle>
       </HeaderContainer>
+      {showMenu && <SmNav handleClose={() => setShowMenu(false)} />}
     </HeaderStyle>
   );
 }
+
+const SmMenu = styled.div`
+  position: fixed;
+  left: 0;
+  top: 102px;
+  width: 100vw;
+  height: calc(100vh - 102px);
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 1px 8px 1px rgba(0, 0, 0, 0.1);
+
+  .content {
+    background-color: #fff;
+    .top {
+      .seed-menu {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+      .seed-menu,
+      .sub-menu,
+      .sub-menu li {
+        padding-inline: 30px;
+      }
+      .seed-menu,
+      .sub-menu li {
+        line-height: 44px;
+        padding-inline: 30p;
+        font-weight: 600;
+        cursor: pointer;
+        a {
+          color: unset;
+          text-decoration: none;
+        }
+      }
+      .sub-menu li:hover {
+        color: #a8e100;
+      }
+    }
+    .bottom {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      justify-content: center;
+      border-top: 1px solid #ddd;
+      margin-top: 20px;
+    }
+  }
+  .mask {
+    flex: 1;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`;
 
 const HeaderStyle = styled.header`
   height: 102px;
@@ -130,6 +256,21 @@ const NavStyle = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
+  @media (max-width: 750px) {
+    display: none;
+  }
+`;
+
+const SmNavStyle = styled.div`
+  display: none;
+  img {
+    position: relative;
+    top: 18px;
+    cursor: pointer;
+  }
+  @media (max-width: 750px) {
+    display: block;
+  }
 `;
 
 const SelectBox = styled.div`
@@ -152,5 +293,12 @@ const EnterButton = styled.div`
   display: flex;
   align-items: center;
   gap: 9px;
+  cursor: pointer;
+`;
+
+const LanguageBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
   cursor: pointer;
 `;
