4f921106865a1059cd3fb7da618d95ee29eb6dec
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-08-13 02:18:43 +0800

    add stars

---
+++ combiner/add_star.py
@@ -0,0 +1,113 @@
+import itertools
+import os
+import random
+import math
+from multiprocessing import Pool
+from PIL import Image, ImageOps
+
+
+def load_images(path):
+    images = []
+    for root, _, files in os.walk(path):
+        for fileName in files:
+            sufix = fileName.rsplit('.')[1]
+            # directory = fileName.rsplit('.')[0]
+            if sufix == 'png':
+                f = os.path.join(root, fileName)
+                # images.append((os.path.basename(fileName), Image.open(f)))
+                img = Image.open(f)
+                print(f, img.size, img.mode)
+                images.append((f, img))
+    return images
+
+
+def load_stars(path):
+    images = []
+    for root, _, files in os.walk(path):
+        for fileName in files:
+            sufix = fileName.rsplit('.')[1]
+            # directory = fileName.rsplit('.')[0]
+            if sufix == 'png' and fileName.contain('-') and not fileName.startswith('W.png'):
+                f = os.path.join(root, fileName)
+                # images.append((os.path.basename(fileName), Image.open(f)))
+                img = Image.open(f)
+                print(f, img.size, img.mode)
+                images.append((f, img))
+    return images
+
+
+def add_star(params):
+    save_name, combination = params
+
+    im = Image.new('RGBA', (3000, 3000))
+
+    background, star = combination
+
+    if background is not None:
+        im = Image.alpha_composite(im, background)
+    if star is not None:
+        im = Image.alpha_composite(im, star)
+
+    # ImageOps.scale(im, 1, Image.NEAREST).save(save_name)
+    im.save(save_name)
+    pass
+
+
+def dup_list(l, num):
+    times = num//len(l)
+    remain = num % len(l)
+    lll = sum(list(map(lambda n: l[:], range(times))), []) + l[:remain]
+    return lll
+    pass
+
+
+def gen_comp(nfts, star_, directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    items = []
+
+    for x in range(len(nfts)):
+        n_nft, nft = nfts[x]
+        n_star, star = star_
+
+        name = os.path.splitext(n_nft)[0].split('-')[1]
+        print(name)
+
+        n_background, n_body, n_eye, _, n_head, n_ring, n_cloth = name.split('#')
+
+        name = '#'.join(list(map(lambda n: os.path.splitext(os.path.basename(n))
+                        [0], [n_background, n_body, n_eye, n_star, n_head, n_ring, n_cloth])))
+
+        save_name = os.path.join(directory, 'seed-' + name + '.png')
+        items.append((save_name, (nft, star)))
+        pass
+
+    print("max number:", len(items))
+    return items
+    pass
+
+
+def merge(items):
+    pool = Pool(processes=4)
+    pool.map(add_star, items)
+    pool.close()
+
+
+def main():
+    nfts = load_images('./meta/seed/')
+    stars = load_stars('./meta/4.star/')
+
+    print(len(nfts), len(stars))
+
+    for star in stars:
+        # nft, star
+        combinations = gen_comp(nfts, star, './final/')
+        merge(combinations)
+        pass
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
