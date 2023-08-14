329494dc97b612bcf892beb46452bf4836dfd565
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-08-15 00:46:40 +0800

    web: update

--- web/src/hooks/useSelectAccout.tsx
+++ web/src/hooks/useSelectAccout.tsx
@@ -2,18 +2,20 @@ import { useWeb3React } from "@web3-react/core";
 import { useAppContext } from "providers/appProvider";
 import { uniPassHooks } from "wallet/connector";
 import { WalletType } from "wallet/wallet";
+import { injected, uniPassWallet } from "wallet/connector";
 
-const { useProvider, useAccount } = uniPassHooks;
+const { useProvider, useAccount, useChainId } = uniPassHooks;
 
 export default function useSelectAccount() {
   const {
     state: { wallet_type },
   } = useAppContext();
 
-  const { account, provider } = useWeb3React();
+  const { account, provider, chainId } = useWeb3React();
 
   const _uprovider = useProvider();
   const _account = useAccount();
+  const _chainId = useChainId();
 
   console.log("=================");
   console.log("wallet_type: ", wallet_type);
@@ -24,12 +26,23 @@ export default function useSelectAccount() {
     _uprovider?.connection?.url,
   );
   console.log("account: ", account, _account);
+  console.log("chainId: ", chainId, _chainId);
   switch (wallet_type) {
     case WalletType.EOA:
-      return { account, provider };
+      return { account, provider, chainId, connector: injected };
     case WalletType.AA:
-      return { account: _account, provider: _uprovider };
+      return {
+        account: _account,
+        provider: _uprovider,
+        chainId: _chainId,
+        connector: uniPassWallet,
+      };
     default:
-      return { account: undefined, provider: undefined };
+      return {
+        account: undefined,
+        provider: undefined,
+        chainId: undefined,
+        connector: undefined,
+      };
   }
 }
