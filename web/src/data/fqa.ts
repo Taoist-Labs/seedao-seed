1a0f4d91b18a63debc447b9f31382e44b351d9ca
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-08-14 16:41:07 +0800

    web: update the fqa

--- web/src/data/fqa.ts
+++ web/src/data/fqa.ts
@@ -1,17 +1,24 @@
+import A1 from "./fqa_zh/1.md";
 import A2 from "./fqa_zh/2.md";
 import A3 from "./fqa_zh/3.md";
 import A4 from "./fqa_zh/4.md";
 import A5 from "./fqa_zh/5.md";
 import A6 from "./fqa_zh/6.md";
 import A7 from "./fqa_zh/7.md";
+import A8 from "./fqa_zh/7.md";
 import A9 from "./fqa_zh/9.md";
+import A10 from "./fqa_zh/10.md";
 
+import A1_EN from "./fqa_en/1.md";
 import A2_EN from "./fqa_en/2.md";
 import A3_EN from "./fqa_en/3.md";
 import A4_EN from "./fqa_en/4.md";
 import A5_EN from "./fqa_en/5.md";
 import A6_EN from "./fqa_en/6.md";
+import A7_EN from "./fqa_en/7.md";
+import A8_EN from "./fqa_en/8.md";
 import A9_EN from "./fqa_en/9.md";
+import A10_EN from "./fqa_en/10.md";
 
 import SGN_A_1 from "./sgn_fqa_zh/1.md";
 import SGN_A_2 from "./sgn_fqa_zh/2.md";
@@ -19,11 +26,15 @@ import SGN_A_3 from "./sgn_fqa_zh/3.md";
 
 export const FQA_LIST_ZH = [
   {
+    name: "我在哪里可以获得 Seed NFT",
+    answer: A1,
+  },
+  {
     name: "没有Seed NFT, 可以加入SeeDAO 吗？",
     answer: A2,
   },
   {
-    name: "Seed NFT有多少特征？",
+    name: "Seed NFT有多少类元素属性？",
     answer: A3,
   },
   {
@@ -35,7 +46,7 @@ export const FQA_LIST_ZH = [
     answer: A5,
   },
   {
-    name: "购买Seed NFT后，我拥有它的版权吗？",
+    name: "Seed NFT 的版权属于我吗？",
     answer: A6,
   },
   {
@@ -43,16 +54,24 @@ export const FQA_LIST_ZH = [
     answer: A7,
   },
   {
-    name: "如何访问我的 Seed NFT 4k 文件？",
-    answer: A9,
+    name: "如何访问我的 Seed NFT 高清图？",
+    answer: A8,
   },
   {
     name: "在哪里可以看到我的Seed NFT？",
     answer: A9,
   },
+  {
+    name: "如何加入 SeeDAO 呢？",
+    answer: A10,
+  },
 ];
 
 export const FQA_LIST_EN = [
+  {
+    name: "How do I get a Seed NFT?",
+    answer: A1_EN,
+  },
   {
     name: "Can I join SeeDAO without a Seed NFT?",
     answer: A2_EN,
@@ -74,12 +93,20 @@ export const FQA_LIST_EN = [
     answer: A6_EN,
   },
   {
-    name: "Seed NFT是什么类型的 NFT？",
-    answer: A7,
+    name: "What kind of NFT is Seed?",
+    answer: A7_EN,
+  },
+  {
+    name: "How can I access my high definition Seed NFT file?",
+    answer: A8_EN,
   },
   {
     name: "Where can I view my Seed NFT?",
     answer: A9_EN,
+  },
+  {
+    name: "How do I join SeeDAO?",
+    answer: A10_EN,
   },
 ];
 
