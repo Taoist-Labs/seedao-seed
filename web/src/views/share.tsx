3a6fe5d5079c9b5bc6d04210bd8ec54f81ed2b01
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-08-16 12:58:24 +0800

    web: update share view

--- web/src/views/share.tsx
+++ web/src/views/share.tsx
@@ -1,10 +1,13 @@
 import styled from "@emotion/styled";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
+import LoaderIcon from "assets/images/loader.svg";
+
 export default function SharePage() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const imgCode = searchParams.get("img");
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     if (!imgCode) {
@@ -18,7 +21,11 @@ export default function SharePage() {
         <img
           src={`https://image-share.fn-labs.workers.dev/${imgCode}.png`}
           alt=""
+          className="img"
+          onLoad={() => setLoaded(true)}
+          style={{ visibility: loaded ? "visible" : "hidden" }}
         />
+        {!loaded && <img src={LoaderIcon} alt="" className="icon" />}
       </div>
     </SharePageStyled>
   );
@@ -34,9 +41,24 @@ const SharePageStyled = styled.div`
     width: 375px;
     border: 8px solid #fff;
     background: #f9d9fb;
-    img {
+    .img {
       width: 100%;
       display: block;
+    }
+  }
+  .icon {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    animation: spin 2.5s linear infinite;
+  }
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
     }
   }
 `;
