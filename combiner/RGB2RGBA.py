2d3886139f209f881bb7b973d4c7c367f924f897
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-08-22 03:35:20 +0800

    add script to convert images

---
+++ combiner/RGB2RGBA.py
@@ -0,0 +1,44 @@
+import itertools
+import os
+import random
+import math
+from multiprocessing import Pool
+from PIL import Image, ImageOps
+
+
+def listFiles(path):
+    filelist = []
+    for root, _, files in os.walk(path):
+        for fileName in files:
+            sufix = fileName.rsplit('.', 1)[1]
+            # directory = fileName.rsplit('.')[0]
+            if sufix == 'png':
+                f = os.path.join(root, fileName)
+                filelist.append(f)
+    return filelist
+
+def convert(filename):
+    img = Image.open(filename)
+    # print(f, img.size, img.mode)
+    if img.mode != 'RGBA':
+        print("not RGBA, convert it", filename, img.size, img.mode)
+        img = img.convert('RGBA')
+        img.save(filename)
+    else:
+        print("RGBA, skip", filename, img.size, img.mode)
+    img.close()
+    pass
+
+def main():
+    files = listFiles('./.tmp/FinalChoose/')
+    print(len(files))
+
+    pool = Pool(processes=16)
+    pool.map(convert, files)
+    pool.close()
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
