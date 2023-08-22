7169958fc3e3d0d0ee3595be331c1a6b654a4845
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-08-23 02:41:06 +0800

    add RGBA2RGB

---
+++ combiner/RGBA2RGB.py
@@ -0,0 +1,50 @@
+import itertools
+import os
+import random
+import math
+from multiprocessing import Pool
+from PIL import Image, ImageOps
+
+
+def listFiles(path):
+    filelist = []
+    for root, _, files in os.walk(path):
+        for fileName in files:
+            sufix = fileName.rsplit('.', 1)[1]
+            # directory = fileName.rsplit('.')[0]
+            if sufix == 'png':
+                f = os.path.join(root, fileName)
+
+                img = Image.open(f)
+                # print(f, img.size, img.mode)
+                if img.mode != 'RGB':
+                    print("not RGB, ", f, img.size, img.mode)
+                    
+                filelist.append(f)
+    return filelist
+
+def convert(filename):
+    img = Image.open(filename)
+    # print(f, img.size, img.mode)
+    if img.mode != 'RGB':
+        print("not RGB, convert it", filename, img.size, img.mode)
+        img = img.convert('RGB')
+        img.save(filename)
+    else:
+        print("RGB, skip", filename, img.size, img.mode)
+    img.close()
+    pass
+
+def main():
+    files = listFiles('./.tmp/FinalChooseWithStars/')
+    print(len(files))
+
+    # pool = Pool(processes=16)
+    # pool.map(convert, files)
+    # pool.close()
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
