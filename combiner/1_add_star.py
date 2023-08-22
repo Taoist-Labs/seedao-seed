c2b0bb5c3d2c003fab696637002d5417d271223d
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-08-22 13:05:41 +0800

    avoid deadlock

--- combiner/1_add_star.py
+++ combiner/1_add_star.py
@@ -15,10 +15,11 @@ def load_images(path):
             if sufix == 'png':
                 f = os.path.join(root, fileName)
                 # images.append((os.path.basename(fileName), Image.open(f)))
-                img = Image.open(f)
+                print(f)
+                # img = Image.open(f)
                 # print(f, img.size, img.mode)
-                images.append((f, img))
-            else: 
+                images.append((f, None))
+            else:
                 print('ignore', fileName)
     return images
 
@@ -33,9 +34,11 @@ def load_stars(path):
             if sufix == 'png':
                 f = os.path.join(root, fileName)
                 # images.append((os.path.basename(fileName), Image.open(f)))
-                img = Image.open(f)
-                print(f, img.size, img.mode)
-                images.append((f, img))
+                # img = Image.open(f)
+                # print(f, img.size, img.mode)
+                images.append((f, None))
+            else:
+                print('ignore', fileName)
     return images
 
 
@@ -44,15 +47,20 @@ def add_star(params):
 
     im = Image.new('RGBA', (3000, 3000))
 
-    background, star = combination
+    backgroundName, starFileName = combination
 
-    if background is not None:
+    if backgroundName is not None:
+        background = Image.open(backgroundName)
         im = Image.alpha_composite(im, background)
-    if star is not None:
+        background.close()
+    if starFileName is not None:
+        star = Image.open(starFileName)
         im = Image.alpha_composite(im, star)
+        star.close()
 
     # ImageOps.scale(im, 1, Image.NEAREST).save(save_name)
     im.save(save_name)
+    im.close()
     pass
 
 
@@ -72,26 +80,26 @@ def gen_comp(nfts, star_, directory):
 
     for x in range(len(nfts)):
         n_nft, nft = nfts[x]
-        n_star, star = star_
+        n_star, _ = star_
 
         name = os.path.splitext(n_nft)[0].split('-')[1]
         # print(name)
 
         # n_background, n_body, n_eye, _, n_head, n_ring, n_cloth = name.split(
         #     '#')
-        n_background, n_body, n_eye, n_star, n_head, n_ring, n_cloth = '', '', '', '', '', '', ''
+        n_background, n_body, n_eye, _, n_head, n_ring, n_cloth = '', '', '', '', '', '', ''
 
         splited = name.split('#')
         if len(splited) == 6:
-            n_background, n_body, n_eye, n_star, n_head, n_cloth = splited
+            n_background, n_body, n_eye, _, n_head, n_cloth = splited
         else:
-            n_background, n_body, n_eye, n_star, n_head, n_ring, n_cloth = splited
+            n_background, n_body, n_eye, _, n_head, n_ring, n_cloth = splited
 
         name = '#'.join(list(map(lambda n: os.path.splitext(os.path.basename(n))
                         [0], [n_background, n_body, n_eye, n_star, n_head, n_ring, n_cloth])))
 
         save_name = os.path.join(directory, 'seed-' + name + '.png')
-        items.append((save_name, (nft, star)))
+        items.append((save_name, (n_nft, n_star)))
         pass
 
     print("max number:", len(items))
@@ -100,21 +108,27 @@ def gen_comp(nfts, star_, directory):
 
 
 def merge(items):
-    pool = Pool(processes=4)
+    pool = Pool(processes=10)
     pool.map(add_star, items)
     pool.close()
 
 
 def main():
-    nfts = load_images('./.tmp/FinalChoose/')
-    stars = load_stars('./meta/4.star/')
+    # nfts = load_images('./.tmp/FinalChoose/')
+    # stars = load_stars('./meta/4.star/')
+    nfts = load_images('/Volumes/“RAMDisk”/FinalChoose')
+    stars = load_stars('/Volumes/“RAMDisk”/4.star')
 
     print(len(nfts), len(stars))
 
+    print(stars)
+
     for star in stars:
         # nft, star
-        combinations = gen_comp(nfts, star, './.tmp/FinalChooseWithStars/')
+        combinations = gen_comp(nfts, star, '/Volumes/“RAMDisk”/FinalChooseWithStars/')
         merge(combinations)
+
+        # list(map(lambda item: add_star(item), combinations))
         pass
 
     pass
