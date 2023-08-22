7a7b1fe729e021ed59723a31c2d564be7ae0be48
xiaosongfu <xiaosong.fu@outlook.com>
2023-08-23 02:03:01 +0800

    contract: update operation scripts

--- contracts/scripts/operation/001_airdrop.ts
+++ contracts/scripts/operation/001_airdrop.ts
@@ -1,6 +1,9 @@
 import { ethers } from "hardhat";
 import deployed from "../deployed";
 import { readAddresses } from "./helper";
+import fs from "fs";
+import * as readline from "node:readline/promises";
+
 async function main() {
   const [signer] = await ethers.getSigners();
   console.log("current signer: ", signer.address);
@@ -10,23 +13,66 @@ async function main() {
     deployed.getSeedMinterContract()
   );
 
-  const addresses = readAddresses(
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  const batchSize = 200;
+  const addresses: string[][] = readAddresses(
     "scripts/operation/001_airdrop_addresses.txt",
-    200
+    batchSize
   );
 
-  console.log(`Calling [SeedMinter]'s airdrop(...) ...`);
-  for (const address of addresses) {
+  console.log(
+    `All batch index is [${Array.from(Array(addresses.length).keys())}]`
+  );
+
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  const batchIndexFile = "scripts/operation/001_airdrop_batchIndex.txt";
+  // read batchIndex from `batchIndexFile`
+  let batchIndex = Number(fs.readFileSync(batchIndexFile, "utf-8").trim());
+
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const answer = await rl.question(
+    `Current batch index is [${batchIndex}], do you want to continue? (y/n) `
+  );
+
+  if (answer.toLowerCase() === "y") {
+    console.log(`Continue with batch index [${batchIndex}]`);
+  } else {
+    console.log(`Exit`);
+    process.exit(0);
+  }
+
+  rl.close();
+
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  for (let i = 0; i < addresses.length; i++) {
+    if (i < batchIndex) {
+      console.log(`Batch index [${i}] skipped`);
+      continue;
+    }
+
     // for (const add of address) {
     //   console.log(`address: ${add}`);
     // }
     // console.log(`------------------------`);
 
-    const tx = await seedMinter.airdrop(address);
+    const tx = await seedMinter.airdrop(addresses[i]);
     await tx.wait();
-    console.log(`airdrop(...) called, tx: ${tx.hash}`);
+    console.log(`Batch index [${i}] executed, tx: ${tx.hash}`);
+    // console.log(`Batch index [${i}] executed`);
+
+    // update batchIndex at `batchIndexFile`
+    batchIndex++;
+    fs.writeFileSync(batchIndexFile, batchIndex.toString(), { flag: "w" });
   }
-  console.log(`Calling [SeedMinter]'s airdrop(...) done`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- contracts/scripts/operation/002_setClaimed.ts
+++ contracts/scripts/operation/002_setClaimed.ts
@@ -1,6 +1,8 @@
 import { ethers } from "hardhat";
 import deployed from "../deployed";
 import { readAddresses } from "./helper";
+import fs from "fs";
+import * as readline from "node:readline/promises";
 
 async function main() {
   const [signer] = await ethers.getSigners();
@@ -11,23 +13,66 @@ async function main() {
     deployed.getSeedMinterContract()
   );
 
-  const addresses = readAddresses(
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  const batchSize = 200;
+  const addresses: string[][] = readAddresses(
     "scripts/operation/002_claimed_addresses.txt",
-    200
+    batchSize
   );
 
-  console.log(`Calling [SeedMinter]'s setClaimed(...) ...`);
-  for (const address of addresses) {
+  console.log(
+    `All batch index is [${Array.from(Array(addresses.length).keys())}]`
+  );
+
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  const batchIndexFile = "scripts/operation/002_claimed_batchIndex.txt";
+  // read batchIndex from `batchIndexFile`
+  let batchIndex = Number(fs.readFileSync(batchIndexFile, "utf-8").trim());
+
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const answer = await rl.question(
+    `Current batch index is [${batchIndex}], do you want to continue? (y/n) `
+  );
+
+  if (answer.toLowerCase() === "y") {
+    console.log(`Continue with batch index [${batchIndex}]`);
+  } else {
+    console.log(`Exit`);
+    process.exit(0);
+  }
+
+  rl.close();
+
+  // -----  -----  -----  -----  -----  -----  -----  -----
+
+  for (let i = 0; i < addresses.length; i++) {
+    if (i < batchIndex) {
+      console.log(`Batch index [${i}] skipped`);
+      continue;
+    }
+
     // for (const add of address) {
     //   console.log(`address: ${add}`);
     // }
     // console.log(`------------------------`);
 
-    const tx = await seedMinter.setClaimed(address);
+    const tx = await seedMinter.setClaimed(addresses[i]);
     await tx.wait();
-    console.log(`setClaimed(...) called, tx: ${tx.hash}`);
+    console.log(`Batch index [${i}] executed, tx: ${tx.hash}`);
+    // console.log(`Batch index [${i}] executed`);
+
+    // update batchIndex at `batchIndexFile`
+    batchIndex++;
+    fs.writeFileSync(batchIndexFile, batchIndex.toString(), { flag: "w" });
   }
-  console.log(`Calling [SeedMinter]'s setClaimed(...) done`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
